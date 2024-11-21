850131954389f7d6c0f8aa3013ad89f7bbf50554
Arthur Santana dos Santos <arthur.santosguitarrista@gmail.com>
2024-11-21 15:57:16 -0300

    Update candidatos.php

--- candidatos.php
+++ candidatos.php
@@ -161,21 +161,15 @@
         </div>
     </header>
 
-<div id="side-menu" class="side-menu">
-<a href="javascript:void(0)" class="close-btn" onclick="closeMenu()">&times;</a>
-        <a href="#">Tutorial</a>
-        <a href="candidatos.php">Candidatos</a>
-        <a href="eventosRecrutador.php">Eventos</a>
-        <a href="editar_perfil_rec.php">Configurações</a>
-
     <div id="side-menu" class="side-menu">
         <a href="javascript:void(0)" class="close-btn" onclick="closeMenu()">&times;</a>
-        <a href="#">Tutorial</a>
+        <a href="tutorial_rec.php">Como usar o sistema</a>
+        
         <a href="candidatos.php">Candidatos</a>
         <a href="eventosRecrutador.php">Meus Eventos</a>
         <a href="postar_evento.php">Adicionar evento</a>
-        <a href="editar_perfil_rec.php">Configurações</a>
-        <a href="sobre_nos.php">Sobre Nós</a>
+        <a href="editar_perfil_rec.php">Editar Perfil</a>
+        <a href="sobre_nos_rec.php">Sobre Nós</a>
         <a href="logout.php">Logout</a>
     </div>
 
